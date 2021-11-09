4816b8d9018bbc9c7bd9b984dc00c3573f7946ea
Anatoliy <72268331+AProkhorevich@users.noreply.github.com>
2021-11-10 00:29:00 +0300

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace LinqFormPMI32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the StudentId editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgStudnets = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dgCourseWork = new System.Windows.Forms.DataGridView();
+            this.btnAfter2000 = new System.Windows.Forms.Button();
+            this.dgMoreThen3 = new System.Windows.Forms.DataGridView();
+            this.StudentsWithSameProfessor = new System.Windows.Forms.Button();
+            this.MarkBiggerThen3 = new System.Windows.Forms.Button();
+            this.dgBornAfter2000 = new System.Windows.Forms.DataGridView();
+            this.dgStudentsWithSameProfessor = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudnets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCourseWork)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMoreThen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBornAfter2000)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudentsWithSameProfessor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1083, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.workerToolStripMenuItem,
+            this.salaryToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // workerToolStripMenuItem
+            // 
+            this.workerToolStripMenuItem.Name = "workerToolStripMenuItem";
+            this.workerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.workerToolStripMenuItem.Text = "Student";
+            this.workerToolStripMenuItem.Click += new System.EventHandler(this.StudentToolStripMenuItem_Click);
+            // 
+            // salaryToolStripMenuItem
+            // 
+            this.salaryToolStripMenuItem.Name = "salaryToolStripMenuItem";
+            this.salaryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salaryToolStripMenuItem.Text = "Сoursework";
+            this.salaryToolStripMenuItem.Click += new System.EventHandler(this.SalaryToolStripMenuItem_Click);
+            // 
+            // dgStudnets
+            // 
+            this.dgStudnets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStudnets.Location = new System.Drawing.Point(28, 46);
+            this.dgStudnets.Name = "dgStudnets";
+            this.dgStudnets.Size = new System.Drawing.Size(456, 168);
+            this.dgStudnets.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dgCourseWork
+            // 
+            this.dgCourseWork.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCourseWork.Location = new System.Drawing.Point(28, 244);
+            this.dgCourseWork.Name = "dgCourseWork";
+            this.dgCourseWork.Size = new System.Drawing.Size(456, 160);
+            this.dgCourseWork.TabIndex = 2;
+            // 
+            // btnAfter2000
+            // 
+            this.btnAfter2000.Location = new System.Drawing.Point(781, 99);
+            this.btnAfter2000.Name = "btnAfter2000";
+            this.btnAfter2000.Size = new System.Drawing.Size(107, 47);
+            this.btnAfter2000.TabIndex = 3;
+            this.btnAfter2000.Text = "Born > 2000";
+            this.btnAfter2000.UseVisualStyleBackColor = true;
+            this.btnAfter2000.Click += new System.EventHandler(this.btnAfter2000_Click);
+            // 
+            // dgMoreThen3
+            // 
+            this.dgMoreThen3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMoreThen3.Location = new System.Drawing.Point(529, 46);
+            this.dgMoreThen3.Name = "dgMoreThen3";
+            this.dgMoreThen3.Size = new System.Drawing.Size(246, 168);
+            this.dgMoreThen3.TabIndex = 4;
+            // 
+            // StudentsWithSameProfessor
+            // 
+            this.StudentsWithSameProfessor.Location = new System.Drawing.Point(781, 152);
+            this.StudentsWithSameProfessor.Name = "StudentsWithSameProfessor";
+            this.StudentsWithSameProfessor.Size = new System.Drawing.Size(107, 47);
+            this.StudentsWithSameProfessor.TabIndex = 5;
+            this.StudentsWithSameProfessor.Text = "Count students with same professor";
+            this.StudentsWithSameProfessor.UseVisualStyleBackColor = true;
+            this.StudentsWithSameProfessor.Click += new System.EventHandler(this.StudentsWithSameProfessor_Click);
+            // 
+            // MarkBiggerThen3
+            // 
+            this.MarkBiggerThen3.Location = new System.Drawing.Point(781, 46);
+            this.MarkBiggerThen3.Name = "MarkBiggerThen3";
+            this.MarkBiggerThen3.Size = new System.Drawing.Size(107, 47);
+            this.MarkBiggerThen3.TabIndex = 6;
+            this.MarkBiggerThen3.Text = "Marks > 3";
+            this.MarkBiggerThen3.UseVisualStyleBackColor = true;
+            this.MarkBiggerThen3.Click += new System.EventHandler(this.MarkBiggerThen3_Click);
+            // 
+            // dgBornAfter2000
+            // 
+            this.dgBornAfter2000.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBornAfter2000.Location = new System.Drawing.Point(529, 244);
+            this.dgBornAfter2000.Name = "dgBornAfter2000";
+            this.dgBornAfter2000.Size = new System.Drawing.Size(246, 160);
+            this.dgBornAfter2000.TabIndex = 7;
+            // 
+            // dgStudentsWithSameProfessor
+            // 
+            this.dgStudentsWithSameProfessor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStudentsWithSameProfessor.Location = new System.Drawing.Point(529, 422);
+            this.dgStudentsWithSameProfessor.Name = "dgStudentsWithSameProfessor";
+            this.dgStudentsWithSameProfessor.Size = new System.Drawing.Size(246, 160);
+            this.dgStudentsWithSameProfessor.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 594);
+            this.Controls.Add(this.dgStudentsWithSameProfessor);
+            this.Controls.Add(this.dgBornAfter2000);
+            this.Controls.Add(this.MarkBiggerThen3);
+            this.Controls.Add(this.StudentsWithSameProfessor);
+            this.Controls.Add(this.dgMoreThen3);
+            this.Controls.Add(this.btnAfter2000);
+            this.Controls.Add(this.dgCourseWork);
+            this.Controls.Add(this.dgStudnets);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudnets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCourseWork)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMoreThen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBornAfter2000)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStudentsWithSameProfessor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salaryToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgStudnets;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dgCourseWork;
+        private System.Windows.Forms.Button btnAfter2000;
+        private System.Windows.Forms.DataGridView dgMoreThen3;
+        private System.Windows.Forms.Button StudentsWithSameProfessor;
+        private System.Windows.Forms.Button MarkBiggerThen3;
+        private System.Windows.Forms.DataGridView dgBornAfter2000;
+        private System.Windows.Forms.DataGridView dgStudentsWithSameProfessor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LinqFormPMI32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            st = new List<Student>();
+            cw = new List<Сoursework>();
+        }
+        class Student
+        {
+            public int Id { set; get; }
+            public string name { set; get; }
+            public int yearOfBirth { set; get; }
+            public int course { set; get; }
+            public Student(int StudentId, string Name,int Year, int Course)
+            {
+                Id = StudentId;
+                name = Name;
+                yearOfBirth = Year;
+                course = Course;
+            }
+            public override string ToString()
+            {
+                return String.Format("{0} {1} {2}", Id, name, course);
+            }
+        }
+        class Сoursework
+        {
+            public int studentId { set; get; }
+            public string professorName{ set; get; }
+            public string title { set; get; }
+            public int mark { get; set; }
+            public Сoursework(int Code, string ProfessorName, string Title, int Mark)
+            {
+                studentId = Code;
+                professorName = ProfessorName;
+                title = Title;
+                mark = Mark;
+            }
+        }
+        List<Student> st = null;
+        List<Сoursework> cw = null;
+        public void readStudents(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                string[] fields;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    fields = line.Split(new char[] { ',' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    Student wr = new Student(int.Parse(fields[0]), fields[1].Trim(),
+                        int.Parse(fields[2]), int.Parse(fields[3]));
+                    st.Add(wr);
+                }
+                dgStudnets.DataSource = st;
+            }
+
+        }
+        public void readCourseWork(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                string[] fields;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    fields = line.Split(new char[] { ',' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    Сoursework cw = new Сoursework(int.Parse(fields[0]),
+                       fields[1], fields[2], int.Parse(fields[3]));
+                    this.cw.Add(cw);
+                }
+                dgCourseWork.DataSource = cw;
+            }
+
+        }
+
+        private void StudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                readStudents(fileName);
+            }
+        }
+
+        private void SalaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                readCourseWork(fileName);
+            }
+        }
+
+        private void MarkBiggerThen3_Click(object sender, EventArgs e)
+        {
+            var StudentsWith4Or5 = from student in st join work in cw 
+                                   on student.Id equals work.studentId
+                                   where work.mark > 3
+                                   select new { fio = student.name, mark = work.mark};
+            dgMoreThen3.DataSource = StudentsWith4Or5.ToList();
+        }
+
+        private void btnAfter2000_Click(object sender, EventArgs e)
+        {
+            var Students = from student in st
+                           where student.yearOfBirth > 2000
+                           select new { name = student.name, fullYears = DateTime.Today.Year - student.yearOfBirth };
+            dgBornAfter2000.DataSource = Students.ToList();
+        }
+
+        private void StudentsWithSameProfessor_Click(object sender, EventArgs e)
+        {
+            var numberOfStudents = cw.Count(item => item.mark)
+            dgStudentsWithSameProfessor.DataSource = numberOfStudents.ToList();
+        }
+
+        //private void Button1_Click(object sender, EventArgs e)
+        //{
+        //    var workersLess35 = from item in st
+        //                        where item.year > DateTime.Now.Year - 35
+        //                        select new { fio = item.name, profession = item.profession };
+        //    dgResult.DataSource = workersLess35.ToList();
+        //}
+
+        //private void Button2_Click(object sender, EventArgs e)
+        //{
+        //    float maxSalary2 = cw.Max(t=>t.salary2);
+        //    //var result = (from it_lw in st
+        //    //              from it_ls in cw
+        //    //              let maxSalary2 = cw.Max(t => t.salary2)
+        //    //              where it_lw.StudentId == it_ls.StudentId && it_ls.salary2 == maxSalary2
+        //    //              select new { fio = it_lw.name,
+        //    //                  department = it_lw.department,
+        //    //                  maxSalary = it_ls.salary2 }).ToList();
+        //    var result = st.Join(cw,
+        //        first => first.code,
+        //        second => second.StudentId,
+        //        (first, second) => new
+        //        {
+        //            fio = first.name,
+        //            department = first.department,
+        //            maxsalary = second.salary2
+        //        }).Where(t => t.maxsalary == maxSalary2).ToList();
+        //    dgResult.DataSource = result;
+        //}
+
+        //private void Button3_Click(object sender, EventArgs e)
+        //{
+        //    var result = from item in st
+        //                 group item by item.department;
+        //    listBox1.Items.Clear();
+        //    foreach(IGrouping<string,Student> gr in result)
+        //    {
+        //        listBox1.Items.Add(gr.Key);
+        //        foreach(var it in gr)
+        //        {
+        //            listBox1.Items.Add("   " + it);
+        //        }
+        //    }
+        //}
+    }
+}
